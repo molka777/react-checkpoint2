e480d1a3cd7a09fdd8fa06fb68612987f217b207
molka777 <56272267+molka777@users.noreply.github.com>
2020-09-26 14:13:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
 
 function App() {
+  let imageInpublic = "/images/imageInPublic.jpeg"
+  let name = prompt("Type in your name");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ border: 'solid 1px black', maxWidth: '100 vw' }}>
+        <h1 className='title green'>
+          Your name here
+</h1>
+        <h3 className='title'>{name}</h3>
+        {
+          [imageInSrc, imageInpublic].map(currentValue => (
+            <div style={{ textAlign: 'center' }}> <img style={{ width: '30%' }} src={currentValue} alt="image" /></div>
+
+          ))
+        }
+
+
+      </div>
+      <video style={{ marginLeft: '35%' }} width='400' height='300' controls>
+
+        <source src="/images/myVideo.mp4" type="video/mp4" />
+
+      </video>
+    </>
   );
 }
 
